cb9c5dfbfd17d5df72f5b1ef028b89ad44d8ccd4
Vladislav <vkoval2002@gmail.com>
2021-01-05 19:25:38 +0300

    BookDAO now works with Hibernate instead of BookRepo

--- src/main/java/com/dawist_o/repository/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dawist_o.repository;
-
-import com.dawist_o.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepo extends JpaRepository<Book, Long> {
-}
