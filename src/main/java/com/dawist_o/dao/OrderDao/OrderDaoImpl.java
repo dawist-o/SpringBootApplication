36b6b9999a22895ed80ae1e3035d205d43c000da
Vladislav <vkoval2002@gmail.com>
2021-01-06 20:04:25 +0300

    Annotations added

--- src/main/java/com/dawist_o/dao/OrderDao/OrderDaoImpl.java
+++ src/main/java/com/dawist_o/dao/OrderDao/OrderDaoImpl.java
@@ -5,16 +5,21 @@ import com.dawist_o.model.Order;
 import com.dawist_o.model.Order;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
+@Repository
+@Transactional
 public class OrderDaoImpl implements OrderDao {
 
-    @Autowired
+    @PersistenceContext
     private Session session;
 
     @Override
